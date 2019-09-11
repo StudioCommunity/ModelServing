1ca95e061a2e8e3c348b36b902d548be18a3690f
Han Wang <wanhan@microsoft.com>
2019-09-11 11:18:21 +0800

    dummy module: sleep for a while

---
+++ src/azureml-studio-score/azureml/studio/score/dummy_module.py
@@ -0,0 +1,29 @@
+import time
+
+from pip._internal import main as pipmain
+pipmain(["install", "click"])
+import click
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+logging.info(f"Dummy module sleeping")
+logger = logging.getLogger(__name__)
+
+
+# ignore extra arguments
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True,
+))
+@click.option('--sleep_time', default=5)
+def run_pipeline(sleep_time):
+    print(f'Sleep time:{sleep_time}')
+    print('Slept at: %s' % time.ctime())
+    time.sleep(sleep_time)
+    print('Woke up at: %s' % time.ctime())
+
+# python -m azureml.studio.score.dummy_module --sleep_time 5
+if __name__ == '__main__':
+    run_pipeline()

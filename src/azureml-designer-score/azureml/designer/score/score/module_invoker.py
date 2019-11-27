636138621c5c3d869263fb147c436ac260024be4
Ming Gu <migu@microsoft.com>
2019-11-27 11:27:14 +0800

    Temp workaround to enable mutil-classification task score result link to evaluation.

--- src/azureml-designer-score/azureml/designer/score/score/module_invoker.py
+++ src/azureml-designer-score/azureml/designer/score/score/module_invoker.py
@@ -7,10 +7,13 @@ import click
 from azureml.studio.core.io.any_directory import AnyDirectory
 from azureml.studio.core.io.image_directory import ImageDirectory
 from azureml.studio.core.io.data_frame_directory import DataFrameDirectory
+from azureml.studio.core.io.data_frame_directory import save_data_frame_to_directory
+from azureml.studio.core.data_frame_schema import DataFrameSchema
+from azureml.designer.model.model_spec.task_type import TaskType
 
 from . import constants
 from .builtin_score_module import BuiltinScoreModule
-from ..utils import ioutils
+from ..utils import ioutils, schema_utils
 from ..logger import get_logger
 
 # python -m azureml.designer.score.module_invoker --trained-model ../dstest/model/tensorflow-minist/ --dataset ../dstest/outputs/mnist/ --scored-dataset ../dstest/outputs/mnist/ouput --append-score-columns-to-output True
@@ -45,7 +48,23 @@ def entrance(trained_model: str, dataset: str, scored_dataset: str, append_score
 
     logger.info(f"output_df =\n{output_df}")
     logger.info(f"dumping to DFD {scored_dataset}")
-    ioutils.save_dfd(output_df, scored_dataset)
+
+    # Temp workaround for DenseNet Demo
+    if score_module.model.task_type == TaskType.MultiClassification:
+        predict_df = output_df
+        _LABEL_NAME = 'label'
+        score_columns = schema_utils.generate_score_column_meta(predict_df=predict_df)
+        label_column_name = _LABEL_NAME if _LABEL_NAME in predict_df.columns else None
+        meta_data = DataFrameSchema(
+            column_attributes=DataFrameSchema.generate_column_attributes(df=predict_df),
+            score_column_names=score_columns,
+            label_column_name=label_column_name
+        )
+        save_data_frame_to_directory(scored_dataset,
+                                     data=predict_df,
+                                     schema=meta_data.to_dict())
+    else:
+        ioutils.save_dfd(output_df, scored_dataset)
 
 
 if __name__ == "__main__":

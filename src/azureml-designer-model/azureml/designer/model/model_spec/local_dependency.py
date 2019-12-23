3ee86212f0869988fd7214eab9a9a07f456f22ef
Ming Gu <migu@microsoft.com>
2019-12-23 20:17:42 +0800

    fix indent error

--- src/azureml-designer-model/azureml/designer/model/model_spec/local_dependency.py
+++ src/azureml-designer-model/azureml/designer/model/model_spec/local_dependency.py
@@ -39,35 +39,35 @@ class LocalDependencyManager(object):
                     shutil.copyfile(src_path, os.path.join(pyfiles_basepath, filename))
                 self.copied_local_dependencies.append(os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, "pyfiles"))
 
-                # Copy directories
-                src_directories = list(filter(lambda x: not x.endswith(".py"), src_abs_paths))
-                if src_directories:
-                    dirname_cnt_dict = {}
-                    for directory in src_directories:
-                        if not os.path.isdir(directory):
-                            raise Exception(f"Only py files and directories are supported, got {directory}")
+            # Copy directories
+            src_directories = list(filter(lambda x: not x.endswith(".py"), src_abs_paths))
+            if src_directories:
+                dirname_cnt_dict = {}
+                for directory in src_directories:
+                    if not os.path.isdir(directory):
+                        raise Exception(f"Only py files and directories are supported, got {directory}")
 
-                for src_dir_path in src_directories:
-                    is_effective = False
-                    dst_dir_name = os.path.split(src_dir_path)[-1]
-                    dirname_cnt_dict[dst_dir_name] = dirname_cnt_dict.get(dst_dir_name, 0) + 1
-                    if dirname_cnt_dict[dst_dir_name] > 1:
-                        dst_dir_name = f"{dst_dir_name}_{dirname_cnt_dict[dst_dir_name] - 1}"
-                    dst_dir_path = os.path.join(temp_dir_path, dst_dir_name)
-                    for sub_item_name in os.listdir(src_dir_path):
-                        src_sub_item_path = os.path.join(src_dir_path, sub_item_name)
-                        dst_sub_item_path = os.path.join(dst_dir_path, sub_item_name)
-                        if os.path.isfile(src_sub_item_path) and sub_item_name.endswith(".py"):
-                            is_effective = True
-                            os.makedirs(dst_dir_path, exist_ok=True)
-                            shutil.copyfile(src_sub_item_path, dst_sub_item_path)
-                        if os.path.isdir(src_sub_item_path) and _is_python_module(src_sub_item_path):
-                            is_effective = True
-                            ioutils.copytree_include(src_sub_item_path, dst_sub_item_path,
-                                                    include_extensions=(".py",), exist_ok=True)
-                    if is_effective:
-                        self.copied_local_dependencies.append(
-                            os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, dst_dir_name))
+            for src_dir_path in src_directories:
+                is_effective = False
+                dst_dir_name = os.path.split(src_dir_path)[-1]
+                dirname_cnt_dict[dst_dir_name] = dirname_cnt_dict.get(dst_dir_name, 0) + 1
+                if dirname_cnt_dict[dst_dir_name] > 1:
+                    dst_dir_name = f"{dst_dir_name}_{dirname_cnt_dict[dst_dir_name] - 1}"
+                dst_dir_path = os.path.join(temp_dir_path, dst_dir_name)
+                for sub_item_name in os.listdir(src_dir_path):
+                    src_sub_item_path = os.path.join(src_dir_path, sub_item_name)
+                    dst_sub_item_path = os.path.join(dst_dir_path, sub_item_name)
+                    if os.path.isfile(src_sub_item_path) and sub_item_name.endswith(".py"):
+                        is_effective = True
+                        os.makedirs(dst_dir_path, exist_ok=True)
+                        shutil.copyfile(src_sub_item_path, dst_sub_item_path)
+                    if os.path.isdir(src_sub_item_path) and _is_python_module(src_sub_item_path):
+                        is_effective = True
+                        ioutils.copytree_include(src_sub_item_path, dst_sub_item_path,
+                                                include_extensions=(".py",), exist_ok=True)
+                if is_effective:
+                    self.copied_local_dependencies.append(
+                        os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, dst_dir_name))
 
             if self.copied_local_dependencies:
                 zip_file_path = os.path.join(artifact_path, ModelSpecConstants.LOCAL_DEPENDENCIES_ZIP_FILE_NAME)

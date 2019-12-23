b3c9156e9c735ae8ecf0fbcfcc45ac925c16179d
Ming Gu <migu@microsoft.com>
2019-12-23 19:32:38 +0800

    Use temp folder

--- src/azureml-designer-model/azureml/designer/model/model_spec/local_dependency.py
+++ src/azureml-designer-model/azureml/designer/model/model_spec/local_dependency.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import tempfile
 
 from ..constants import ModelSpecConstants
 from ..utils import ioutils, ziputils
@@ -24,70 +25,53 @@ class LocalDependencyManager(object):
 
     def save(self, artifact_path, exist_ok=True) -> list:
         src_abs_paths = [os.path.abspath(_) for _ in self.local_dependencies]
-        dst_root_dir = os.path.join(artifact_path, ModelSpecConstants.LOCAL_DEPENDENCIES_PATH)
-        if os.path.exists(dst_root_dir) and not exist_ok:
-            raise Exception(f"{dst_root_dir} exists, please set exist_ok=True if you want to overwrite.")
 
-        # Copy pyfiles
-        src_py_files = list(filter(lambda x: x.endswith(".py"), src_abs_paths))
-        if src_py_files:
-            py_filenames = [os.path.split(file_path)[-1] for file_path in src_py_files]
-            if len(set(py_filenames)) < len(py_filenames):
-                raise Exception("There are duplication in dependency py file name, which is not allowed.")
-            pyfiles_basepath = os.path.join(dst_root_dir, "pyfiles")
-            os.makedirs(pyfiles_basepath, exist_ok=True)
-            for filename, src_path in zip(py_filenames, src_py_files):
-                shutil.copyfile(src_path, os.path.join(pyfiles_basepath, filename))
-            self.copied_local_dependencies.append(os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, "pyfiles"))
+        with tempfile.TemporaryDirectory() as temp_dir_path:
+            # Copy pyfiles
+            src_py_files = list(filter(lambda x: x.endswith(".py"), src_abs_paths))
+            if src_py_files:
+                py_filenames = [os.path.split(file_path)[-1] for file_path in src_py_files]
+                if len(set(py_filenames)) < len(py_filenames):
+                    raise Exception("There are duplication in dependency py file name, which is not allowed.")
+                pyfiles_basepath = os.path.join(temp_dir_path, "pyfiles")
+                os.makedirs(pyfiles_basepath, exist_ok=True)
+                for filename, src_path in zip(py_filenames, src_py_files):
+                    shutil.copyfile(src_path, os.path.join(pyfiles_basepath, filename))
+                self.copied_local_dependencies.append(os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, "pyfiles"))
 
-        # Copy directories
-        src_directories = list(filter(lambda x: not x.endswith(".py"), src_abs_paths))
-        if src_directories:
-            dirname_cnt_dict = {}
-            for directory in src_directories:
-                if not os.path.isdir(directory):
-                    raise Exception(f"Only py files and directories are supported, got {directory}")
+                # Copy directories
+                src_directories = list(filter(lambda x: not x.endswith(".py"), src_abs_paths))
+                if src_directories:
+                    dirname_cnt_dict = {}
+                    for directory in src_directories:
+                        if not os.path.isdir(directory):
+                            raise Exception(f"Only py files and directories are supported, got {directory}")
 
-            for src_dir_path in src_directories:
-                is_effective = False
-                dst_dir_name = os.path.split(src_dir_path)[-1]
-                dirname_cnt_dict[dst_dir_name] = dirname_cnt_dict.get(dst_dir_name, 0) + 1
-                if dirname_cnt_dict[dst_dir_name] > 1:
-                    dst_dir_name = f"{dst_dir_name}_{dirname_cnt_dict[dst_dir_name] - 1}"
-                dst_dir_path = os.path.join(dst_root_dir, dst_dir_name)
-                for sub_item_name in os.listdir(src_dir_path):
-                    src_sub_item_path = os.path.join(src_dir_path, sub_item_name)
-                    dst_sub_item_path = os.path.join(dst_dir_path, sub_item_name)
-                    if os.path.isfile(src_sub_item_path) and sub_item_name.endswith(".py"):
-                        is_effective = True
-                        os.makedirs(dst_dir_path, exist_ok=True)
-                        shutil.copyfile(src_sub_item_path, dst_sub_item_path)
-                    if os.path.isdir(src_sub_item_path) and _is_python_module(src_sub_item_path):
-                        is_effective = True
-                        ioutils.copytree_include(src_sub_item_path, dst_sub_item_path,
-                                                 include_extensions=(".py",), exist_ok=True)
-                if is_effective:
-                    self.copied_local_dependencies.append(
-                        os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, dst_dir_name))
+                for src_dir_path in src_directories:
+                    is_effective = False
+                    dst_dir_name = os.path.split(src_dir_path)[-1]
+                    dirname_cnt_dict[dst_dir_name] = dirname_cnt_dict.get(dst_dir_name, 0) + 1
+                    if dirname_cnt_dict[dst_dir_name] > 1:
+                        dst_dir_name = f"{dst_dir_name}_{dirname_cnt_dict[dst_dir_name] - 1}"
+                    dst_dir_path = os.path.join(temp_dir_path, dst_dir_name)
+                    for sub_item_name in os.listdir(src_dir_path):
+                        src_sub_item_path = os.path.join(src_dir_path, sub_item_name)
+                        dst_sub_item_path = os.path.join(dst_dir_path, sub_item_name)
+                        if os.path.isfile(src_sub_item_path) and sub_item_name.endswith(".py"):
+                            is_effective = True
+                            os.makedirs(dst_dir_path, exist_ok=True)
+                            shutil.copyfile(src_sub_item_path, dst_sub_item_path)
+                        if os.path.isdir(src_sub_item_path) and _is_python_module(src_sub_item_path):
+                            is_effective = True
+                            ioutils.copytree_include(src_sub_item_path, dst_sub_item_path,
+                                                    include_extensions=(".py",), exist_ok=True)
+                    if is_effective:
+                        self.copied_local_dependencies.append(
+                            os.path.join(ModelSpecConstants.LOCAL_DEPENDENCIES_PATH, dst_dir_name))
 
-        if os.path.isdir(dst_root_dir):
-            zip_file_path = os.path.join(artifact_path, ModelSpecConstants.LOCAL_DEPENDENCIES_ZIP_FILE_NAME)
-            ziputils.zip_dir(dst_root_dir, zip_file_path)
-            logger.info(f"dst_root_dir = {dst_root_dir}")
-            logger.info(f"type(dst_root_dir) = {type(dst_root_dir)}")
-            logger.info(f"Zipped local_dependencies into {zip_file_path}. Removing original directory.")
-            logger.info(f"Before rmtree: {os.listdir(os.path.dirname(dst_root_dir))}")
-            shutil.rmtree(dst_root_dir)
-            logger.info(f"{dst_root_dir} removed.")
-            logger.info(f"After rmtree: {os.listdir(os.path.dirname(dst_root_dir))}")
-            logger.info(f"Writing Test File.")
-            test_file_path = os.path.join(os.path.dirname(dst_root_dir), "test.txt")
-            with open(test_file_path, 'w') as fp:
-                fp.write("Hello World\n")
-
-            logger.info(f"Before remove file: {os.listdir(os.path.dirname(zip_file_path))}")
-            os.remove(test_file_path)
-            logger.info(f"After remove file: {os.listdir(os.path.dirname(zip_file_path))}")
+            if self.copied_local_dependencies:
+                zip_file_path = os.path.join(artifact_path, ModelSpecConstants.LOCAL_DEPENDENCIES_ZIP_FILE_NAME)
+                ziputils.zip_dir(temp_dir_path, zip_file_path)
 
     def load(self, artifact_path, relative_paths):
         self.local_dependencies = [os.path.abspath(os.path.join(artifact_path, path)) for path in relative_paths]
